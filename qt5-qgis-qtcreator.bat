1e8cb0d001038d7e3f4961d93d7e4e6d20c4b132
Nathan Woodrow <madmanwoo@gmail.com>
2017-02-09 14:58:49 +1000

    Fix creator load

--- qt5-qgis-qtcreator.bat
+++ qt5-qgis-qtcreator.bat
@@ -4,4 +4,5 @@ ECHO Setting up QGIS DEV ENV
 
 CALL %~dp0qt5-env-qgis.bat
 
-start "Qt Creator" /B "qtcreator.exe" %*
+set QT_PLUGIN_PATH=C:\Qt\Tools\QtCreator\bin\plugins;%QT_PLUGIN_PATH%
+start "Qt Creator" /B "C:\Qt\Tools\QtCreator\bin\qtcreator.exe" %*
